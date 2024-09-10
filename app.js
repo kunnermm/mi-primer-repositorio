b30428c572047d31f82a69d2fbae2c28e991d0a7
Monica Gonzalez <monica.gonzalez.ramirez@gmail.com>
2024-09-09 19:22:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const API_KEY = "AIzaSyBz0D7m-EXAMPLEKEY1234567";
+// Clave API de Google simulada
+const API_KEY = "AIzaSyD-EXAMPLEKEY1234567";
 console.log("La API Key es: " + API_KEY);
 
+// Token de GitHub simulado
 const TOKEN = "ghp_1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 console.log("El token es: " + TOKEN);
 
-const KEY = "1234qwert";
-console.log("La key es: " + KEY);
+// Clave de acceso de AWS simulada
+const AWS_KEY = "AKIAIOSFODNN7EXAMPLE";
+console.log("La AWS Key es: " + AWS_KEY);
+
+// Clave secreta de AWS simulada
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+console.log("La AWS Secret Access Key es: " + AWS_SECRET_ACCESS_KEY);
+
+// URL con clave de acceso simulada
+const URL_WITH_KEY = "https://api.postman.com/collections/34288329-3cf8d09c-1d2a-43fd-9d0d-9f4f3eab6550?access_key=EXAMPLEKEY";
+console.log("La URL con clave es: " + URL_WITH_KEY);
+
